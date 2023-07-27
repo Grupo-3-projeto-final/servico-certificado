2a63fc8e3f4ff14d33606ad46458b059bd0bcbeb
ricardo55991 <ricardo.barros.jr@gmail.com>
2023-07-27 18:21:19 -0300

    Ajuste controller

--- ui/Program.cs
+++ ui/Program.cs
@@ -1,28 +1,32 @@
+using servico_certificado.Application.Entities;
 using servico_certificado.Domain.interfaces;
 using servico_certificado.Infrastructure.Utilities;
-using servico_certificado.Web.Routes;
-using WkHtmlToPdfDotNet.Contracts;
 using WkHtmlToPdfDotNet;
+using WkHtmlToPdfDotNet.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Configuration.AddEnvironmentVariables(); // Carrega as variáveis de ambiente
+
 // Injeção de dependência
 builder.Services.AddScoped<IHtmlParaPdf, HtmlParaPdf>();
+builder.Services.AddScoped<CertificadoService>();
 builder.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
-
-new GerarCertificadoRoute(app).Register();
+app.MapControllers();
 
 app.Run();
